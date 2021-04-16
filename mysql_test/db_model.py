8ae30eaaf373b2b569892169281a2c3bcff82d07
dangzhangjing <dangzhangjing@baijia.com>
2021-04-16 21:17:09 +0800

    查找指定的数据

--- mysql_test/db_model.py
+++ mysql_test/db_model.py
@@ -3,7 +3,7 @@ import pymysql
 
 class db_utils:
     #连接数据库
-    conn = pymysql.connect(
+    mysql_conn = pymysql.connect(
         host = mysql_test.db_config.host,
         port = mysql_test.db_config.port,
         user = mysql_test.db_config.user,
@@ -11,29 +11,39 @@ class db_utils:
         db = mysql_test.db_config.db,
         charset = mysql_test.db_config.charset
     )
-    cursor = conn.cursor()
+
     my_sql = "select name,sex from user "
+    cursor = mysql_conn.cursor()
     cursor.execute(my_sql)
-    #one = cursor.fetchone()
-    #many = cursor.fetchmany(2)
-    all = cursor.fetchall()
-    count = cursor.rowcount
 
 
-    #打印sql执行结果
-    def Print(self):
-        #print(db_utils.one)
-        #print(db_utils.many)
-        print("sql执行结果：{}".format(db_utils.all))
+    #获取sql执行受影响的行数
+    def get_mysql_count(self):
+        count = self.cursor.rowcount
+        return count
+
+    def print_mysql_count(self):
+        print("sql执行结果受影响的行数：{}".format(self.get_mysql_count()))
+
+
+    #获取sql执行结果
+    def get_mysql_result(self):
+        sql_result = self.cursor.fetchall()
+        return sql_result
+
+    def print_mysql_result(self):
+        print("sql执行结果集：{}".format(self.get_mysql_result()))
 
     #查找性别为女生的人员
-    def Find(self):
+    def find(self):
         print("查找出的女生为：")
-        for i in range(db_utils.count):
-            a,b  = db_utils.all[i]
+        mysql_result = self.get_mysql_result()
+        for i in range(self.get_mysql_count()):
+            a,b  = mysql_result[i]
             if b == '女':
                 print("我的名字是{},我是一个{}生".format(a,b))
-
-
     cursor.close()
-    conn.close()
+    mysql_conn.close()
+
+    #one = cursor.fetchone()
+    #many = cursor.fetchmany(2)
--- mysql_test/test_start.py
+++ mysql_test/test_start.py
@@ -1,7 +1,11 @@
 
 from mysql_test.db_model import db_utils
 
+
+
+
 if __name__ == '__main__':
-    number = db_utils()
-    number.Print()
-    number.Find()
+    sql_result = db_utils()
+    sql_result.print_mysql_count()
+    #sql_result.print_mysql_result()
+    sql_result.find()
